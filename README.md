39ecbcf8532e2f64e446052a56646b1f28358bed
Ben Southgate <bsouthga@gmail.com>
2017-09-19 17:07:05 -0400

    feat: add readme info

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Generated TypeScript definition files for slate
 
+## Using the typings
+
+First install the typings via npm. For example, to install the `slate` typings,
+we would run:
+
+```shell
+npm install @crosslead/slate-typings
+```
+
+Next, add `"./node_modules/@crosslead"` to the `typeRoots` option in your `tsconfig.json`
+
+```json
+{
+  ...,
+  "typeRoots": ["./node_modules/@crosslead", "@types"]
+}
+```
+
 ## Typings
 
 see [`./packages`](./packages) for the type definition files.
--- scripts/generate-package-json.js
+++ scripts/generate-package-json.js
@@ -1,14 +1,14 @@
 const [_, __, package] = process.argv;
-
 const package$json = require(`../slate/packages/${package}/package.json`);
 const this$package$json = require('../package.json');
 
 console.log(JSON.stringify({
-  name: `${package$json.name}-typings`,
+  name: `@crosslead/${package$json.name}-typings`,
   version: package$json.version,
   decription: `generated typings for ${package$json.name}`,
   repository: this$package$json.repository,
   license: this$package$json.license,
   main: 'index.js',
-  typings: `${package$json.name}.d.ts`
+  typings: `${package$json.name}.d.ts`,
+  private: false
 }, null, 2));
